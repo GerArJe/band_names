ac2c7bfbaac6d8bec97b8ec7a48b2de9a93eea5a
German <germanarevalojerez@gmail.com>
2021-09-13 13:50:19 -0500

    status

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,18 @@
+import 'package:band_names/pages/status.dart';
 import 'package:flutter/material.dart';
- 
+
 import 'package:band_names/pages/home.dart';
 
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      initialRoute: 'home',
-      routes: {
-        'home': (_) => HomePage()
-      },
+      initialRoute: 'status',
+      routes: {'home': (_) => const HomePage(), 'status': (_) => const StatusPage()},
     );
   }
 }
