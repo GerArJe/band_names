a21d36a15ad639b8af6c4a995f0e8673ab423ddd
German <germanarevalojerez@gmail.com>
2021-09-16 10:51:07 -0500

    hear bands

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -15,11 +15,29 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<Band> bands = [
-    Band(id: '1', name: 'Metalica', votes: 5),
-    Band(id: '2', name: 'Queen', votes: 5),
-    Band(id: '3', name: 'Heroes', votes: 5),
-    Band(id: '4', name: 'Bon', votes: 5),
+    // Band(id: '1', name: 'Metalica', votes: 5),
+    // Band(id: '2', name: 'Queen', votes: 5),
+    // Band(id: '3', name: 'Heroes', votes: 5),
+    // Band(id: '4', name: 'Bon', votes: 5),
   ];
+
+  @override
+  void initState() {
+    final socketService = Provider.of<SocketService>(context, listen: false);
+
+    socketService.socket.on('active-bands', (payload) {
+      bands = (payload as List).map((band) => Band.fromMap(band)).toList();
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    final socketService = Provider.of<SocketService>(context, listen: false);
+    socketService.socket.off('active-bands');
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
